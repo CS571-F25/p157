65773b96ea889ab70ac9aa57478861b9f944bba0
Martin Diges <mdiges@wisc.edu>
2025-11-19 21:35:21 -0600

    Updated the About page to contain more details

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,5 +4,6 @@ export default function AboutMe({ isDarkMode })
     return <div style={{ color: isDarkMode ? 'rgba(255, 255, 255, 0.87)' : '#213547' }}>
         <img src={"https://pbs.twimg.com/profile_images/1852330841319280640/6D8HxYmd_400x400.jpg"} alt="Smiling doge" />
         <p>Martin Diges, the author of this page, is quite forgetful and likes to know where their things are.</p>
+        <p>This website was produed in large part through interaction with Cursor. Uses React Router for navigation and React Bootstrap for styling.</p>
     </div> 
 }
